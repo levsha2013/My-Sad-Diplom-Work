02fdde2f35396a206e0376ebe80dffa9434cd9ff
levsha_main <crylov.l@yandex.ru>
2022-02-15 18:14:54 +0300

    Add func for take color from CC(1 or 2) and generate CC-model

--- main.py
+++ main.py
@@ -28,24 +28,58 @@ def one_fix_color_change():
     cv2.imshow('2', result)
     cv2.waitKey(0)
 
+# get color from image CC1
+def get_color_from_CC1(CC1):
+    output = []
+    for j in range(4):
+        for i in range(6):
+            output.append(CC1[200+180*j][200+180*i])
+    return output
 
-img1 = cv2.imread('CC2.jpg')
+# get color from image CC2
+def get_color_from_CC2(CC2):
+    output = []
+    for j in range(4):
+        for i in range(6):
+            output.append(CC2[150+223*j][150+223*i])
+    return output
 
-img1 = cv2.resize(img1,(img1.shape[1]//2, img1.shape[0]//2))    # уменьшаю, чтобы влезло в экран
-img = img1[:472,:]      # убираю надпись снизу
+# find the same f_color(+-step) and change it
+def try_1_to_change(img, f_color, step = 15):
+    for j in range(img.shape[0]):
+        for i in range(img.shape[1]):
+            if(img[j][i][0] <= f_color[0]+step) and (img[j][i][0] >= f_color[0]-step) \
+                    and (img[j][i][1] <= f_color[1]+step) and (img[j][i][1] >= f_color[1]-step)  \
+                    and (img[j][i][2] <= f_color[2]+step) and (img[j][i][2] >= f_color[2]-step):
+                img[j,i,0] = 0
+                img[j,i,1] = 255
+                img[j,i,2] = 255
+    return img
+
+# generate CC
+def generate_CC(colors):
+    output = np.full((600, 900, 3), (0, 0, 0), dtype='uint8')  # create color
+    num_color = 0
+    for i in range(0,600,150):
+        for j in range(0,900,150):
+            output[i:i + 150, j:j + 150, 0] = f_color[num_color][0]
+            output[i:i + 150, j:j + 150, 1] = f_color[num_color][1]
+            output[i:i + 150, j:j + 150, 2] = f_color[num_color][2]
+            num_color+=1
+    return output
 
 
-#cv2.imshow('1 image', img1)
-#cv2.imshow('2 image', img)
+CC1 = cv2.imread('CC1.jpg') # с цифрами
+CC2 = cv2.imread('CC2.jpg') # без цифр
 
-s = set()
-d = dict()
-for i in range(0,img1.shape[0],10):
-    for j in range(0,img1.shape[1],10):
-        #if not img1[i][j][0] in s:
-            #s.add(img1[i][j][0])
-        pass
-#print(s)
-#cv2.imshow('test', test_img)
 
-# создаем цвет и искажаем его шумом
+f_color = get_color_from_CC2(CC2)            # цвет, который ищем
+output = generate_CC(f_color)
+cv2.imshow('123',output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+f_color = get_color_from_CC1(CC1)            # цвет, который ищем
+output = generate_CC(f_color)
+cv2.imshow('123',output)
+cv2.waitKey(0)
