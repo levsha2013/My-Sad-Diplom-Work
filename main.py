0e2793595a8ca8a26f9862a5ec8a82fba1c1bcdb
levsha_main <crylov.l@yandex.ru>
2022-02-17 01:38:25 +0300

    Complite the correct distortion function

--- main.py
+++ main.py
@@ -1,85 +1,55 @@
 import cv2
 import numpy as np
-import random
 
-# distortion
-def gasuss_noise(img=0):
-    noise = np.random.normal(5, 0.5, 500)  # main, SKO, numbers
-    v = np.var(noise)  # disper
-    m = np.mean(noise)
-    #print(m, v, "\n")
-    return noise
 
-# change one fix color
-def one_fix_color_change():
-    start = np.full((400, 400, 3), (60, 54, 175), dtype='uint8')  # create color
-    noise = np.random.normal(0, 20, 3)  # main, SKO, numbers
-    result = np.full((400, 400, 3), (60, 54, 175), dtype='uint8')  # create ndarray for change
-
-    result[:, :, 2] = start[:, :, 2] + noise[2]  # change R
-    result[:, :, 1] = start[:, :, 1] + noise[1]  # change G
-    result[:, :, 0] = start[:, :, 0] + noise[0]  # change B
-
-    print(start[0][0])  # start color
-    print(noise)  # noise
-    print(result[0][0])  # result color
-
-    cv2.imshow('1', start)
-    cv2.imshow('2', result)
-    cv2.waitKey(0)
-
-# get color from image CC1
-def get_color_from_CC1(CC1):
+# get color from image CC1 or CC2
+def get_color_from_CC(img):
     output = []
-    for j in range(4):
-        for i in range(6):
-            output.append(CC1[200+180*j][200+180*i])
+    if img.shape[1] == 1200:
+        for j in range(4):
+            for i in range(6):
+                output.append(img[200 + 180 * j][200 + 180 * i])
+    else:
+        for j in range(4):
+            for i in range(6):
+                output.append(img[150 + 223 * j][150 + 223 * i])
     return output
-
-# get color from image CC2
-def get_color_from_CC2(CC2):
-    output = []
-    for j in range(4):
-        for i in range(6):
-            output.append(CC2[150+223*j][150+223*i])
-    return output
-
-# find the same f_color(+-step) and change it
-def try_1_to_change(img, f_color, step = 15):
-    for j in range(img.shape[0]):
-        for i in range(img.shape[1]):
-            if(img[j][i][0] <= f_color[0]+step) and (img[j][i][0] >= f_color[0]-step) \
-                    and (img[j][i][1] <= f_color[1]+step) and (img[j][i][1] >= f_color[1]-step)  \
-                    and (img[j][i][2] <= f_color[2]+step) and (img[j][i][2] >= f_color[2]-step):
-                img[j,i,0] = 0
-                img[j,i,1] = 255
-                img[j,i,2] = 255
-    return img
 
 # generate CC
-def generate_CC(colors):
+def generate_CC(colors, mode = 0):
     output = np.full((600, 900, 3), (0, 0, 0), dtype='uint8')  # create color
     num_color = 0
-    for i in range(0,600,150):
-        for j in range(0,900,150):
-            output[i:i + 150, j:j + 150, 0] = f_color[num_color][0]
-            output[i:i + 150, j:j + 150, 1] = f_color[num_color][1]
-            output[i:i + 150, j:j + 150, 2] = f_color[num_color][2]
-            num_color+=1
+    for i in range(0, 600, 150):
+        for j in range(0, 900, 150):
+            output[i:i + 150, j:j + 150, 0] = colors[num_color][0]
+            output[i:i + 150, j:j + 150, 1] = colors[num_color][1]
+            output[i:i + 150, j:j + 150, 2] = colors[num_color][2]
+            num_color += 1
+    if mode == 0: cv2.imshow('CC', output)
+    if mode == 1: cv2.imshow('CC_distortion', output)
+    cv2.waitKey(0)
     return output
 
+# incorrect distortion
+def distortion(f_color, m = 0, d = 5):
+    noise = np.random.normal(m, d, 24 * 3)  # main, SKO, numbers (24 colors B,G,R)
+    n_color = []        # sample for new distorted colors
+    for i in range(24):     # 24 colors
+        n_color.append((np.ndarray((3), dtype='uint8')))    # add a new color
+        for j in range(3):      # 3 channels
+            if int(f_color[i][j]) + int(noise[i * 3]) > 255: n_color[i][j] = 255    # if sum more than 255
+            elif int(f_color[i][j]) + int(noise[i * 3]) <0: n_color[i][j] = 0       # if sum less than 0
+            else: n_color[i][j] = f_color[i][j] + noise[i * 3+j]                    # other cases
+    return n_color
 
-CC1 = cv2.imread('CC1.jpg') # с цифрами
-CC2 = cv2.imread('CC2.jpg') # без цифр
 
+CC1 = cv2.imread('CC1.jpg')  # с цифрами
+CC2 = cv2.imread('CC2.jpg')  # без цифр
 
-f_color = get_color_from_CC2(CC2)            # цвет, который ищем
-output = generate_CC(f_color)
-cv2.imshow('123',output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+f_color = get_color_from_CC(CC2)  # цвет, который ищем
+generate_CC(f_color)  # generate CC
 
-f_color = get_color_from_CC1(CC1)            # цвет, который ищем
-output = generate_CC(f_color)
-cv2.imshow('123',output)
-cv2.waitKey(0)
+m = 0
+disp = 50
+n_color = distortion(f_color, m, disp)
+generate_CC(n_color,1)  # generate distortion CC (mode = 1)
